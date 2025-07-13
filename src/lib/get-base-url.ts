8c7e22310a30e3f3b27c5d608b7a47ba76223a6c
Kydollar <rizkywidodocuan@gmail.com>
2025-07-13 17:06:24 +0700

    chore(env): fix a digest property

--- src/lib/get-base-url.ts
+++ src/lib/get-base-url.ts
@@ -5,11 +5,7 @@ const DEV_URL = 'http://localhost:3000';
 export default function getBaseUrl(): string {
   const isDev = process.env.NODE_ENV === 'development';
 
-  const baseUrl = isDev ? DEV_URL : process.env.NEXT_PUBLIC_URL;
-
-  if (!baseUrl) {
-    throw new Error('NEXT_PUBLIC_URL is not defined in environment variables.');
-  }
+  const baseUrl = isDev ? DEV_URL : process.env.NEXT_PUBLIC_URL || DEV_URL;
 
   return trimTrailingSlash(baseUrl);
 }
